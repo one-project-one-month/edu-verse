06a7ac009f41900c304d2a873d2b04995075a46e
naingminkhant <tharkhant777@gmail.com>
2024-05-14 13:18:55 +0630

    update data.sql file and tag&operation for AdminController

--- eduverse/src/main/java/dev/backend/eduverse/controller/AdminController.java
+++ eduverse/src/main/java/dev/backend/eduverse/controller/AdminController.java
@@ -5,7 +5,9 @@ import dev.backend.eduverse.service.AdminService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+
 import java.util.List;
+
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,64 +16,60 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admins")
+@Tag(name = "Admin Operation", description = "REST API CRUD operation for Admin Entity")
 public class AdminController {
 
-  @InitBinder
-  public void initBinder(WebDataBinder dataBinder) {
-    dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
-  }
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
 
-  private final AdminService adminService;
+    private final AdminService adminService;
 
-  public AdminController(AdminService adminService) {
-    this.adminService = adminService;
-  }
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 
-  // Get All
-  @Tag(name = "Get All Admins")
-  @Operation(tags = "Admin Operation")
-  @GetMapping("")
-  public ResponseEntity<List<AdminDto>> getAllAdmins() {
-    List<AdminDto> admins = adminService.findAll();
+    // Get All
+    @Operation(summary = "Get All Admins")
+    @GetMapping("")
+    public ResponseEntity<List<AdminDto>> getAllAdmins() {
+        List<AdminDto> admins = adminService.findAll();
 
-    return new ResponseEntity<>(admins, HttpStatus.OK);
-  }
+        return new ResponseEntity<>(admins, HttpStatus.OK);
+    }
 
-  // Get By Id
-  @Tag(name = "Get Admin By Id")
-  @Operation(tags = "Admin Operation")
-  @GetMapping("/{id}")
-  public ResponseEntity<AdminDto> getById(@PathVariable Long id) {
-    AdminDto adminDto = adminService.findById(id);
+    // Get By Id
+    @Operation(summary = "Get Admin By Id")
+    @GetMapping("/{id}")
+    public ResponseEntity<AdminDto> getById(@PathVariable Long id) {
+        AdminDto adminDto = adminService.findById(id);
 
-    return new ResponseEntity<>(adminDto, HttpStatus.OK);
-  }
+        return new ResponseEntity<>(adminDto, HttpStatus.OK);
+    }
 
-  // Create new
-  @Tag(name = "Create Admin")
-  @Operation(tags = "Admin Operation")
-  @PostMapping("")
-  public ResponseEntity<AdminDto> createAdmin(@Valid @RequestBody AdminDto adminDto) {
-    AdminDto createdAdmin = adminService.createAdmin(adminDto);
-    return new ResponseEntity<>(createdAdmin, HttpStatus.OK);
-  }
+    // Create new
+    @Operation(summary = "Create new Admin")
+    @PostMapping("")
+    public ResponseEntity<AdminDto> createAdmin(@Valid @RequestBody AdminDto adminDto) {
+        AdminDto createdAdmin = adminService.createAdmin(adminDto);
+        return new ResponseEntity<>(createdAdmin, HttpStatus.OK);
+    }
 
-  // Update
-  @Tag(name = "Update Admin")
-  @Operation(tags = "Admin Operation")
-  @PutMapping("/{id}")
-  public ResponseEntity<AdminDto> updateAdmin(
-      @Valid @RequestBody AdminDto adminDto, @PathVariable Long id) {
-    AdminDto updatedAdminDto = adminService.updateAdmin(adminDto, id);
-    return new ResponseEntity<>(updatedAdminDto, HttpStatus.OK);
-  }
+    // Update
+    @Operation(summary = "Update Admin By Id")
+    @PutMapping("/{id}")
+    public ResponseEntity<AdminDto> updateAdmin(
+            @Valid @RequestBody AdminDto adminDto, @PathVariable Long id) {
+        AdminDto updatedAdminDto = adminService.updateAdmin(adminDto, id);
+        return new ResponseEntity<>(updatedAdminDto, HttpStatus.OK);
+    }
 
-  // Delete by Id
-  @Tag(name = "Delete Admin")
-  @Operation(tags = "Admin Operation")
-  @DeleteMapping("/{id}")
-  public ResponseEntity<String> deleteAdmin(@PathVariable Long id) {
-    adminService.deleteById(id);
-    return new ResponseEntity<>("Admin is successfully deleted.", HttpStatus.NO_CONTENT);
-  }
+    // Delete by Id
+    @Operation(summary = "Delete Admin By Id")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAdmin(@PathVariable Long id) {
+        adminService.deleteById(id);
+        return new ResponseEntity<>("Admin is successfully deleted.", HttpStatus.NO_CONTENT);
+    }
 }
